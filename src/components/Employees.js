7fc6788d2ec055fc5c573e1e5bc07b9b03e80eb1
josmy <josmyjoseph33@gmail.com>
2023-02-05 03:01:35 +0530

    first commit

---
+++ src/components/Employees.js
@@ -0,0 +1,16 @@
+const Employees = [
+    {
+        id:"1",
+        Name:"Ajeesh",
+        DOB:"1994-02-24"
+    },{
+        id:"2",
+        Name:"Abhijith",
+        DOB:"1991-09-04"
+    },{
+        id:"3",
+        Name:"Ammu",
+        DOB:"1992-06-27"
+    }
+]
+export default Employees;
---
+++ src/components/Home.js
@@ -0,0 +1,85 @@
+import React, { Fragment } from 'react';
+import {Button, Table} from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Employees from './Employees';
+import {Link,useNavigate} from 'react-router-dom';
+
+function Home()
+{
+    console.log(Employees)
+    let history = useNavigate();
+
+    const handleEdit = (id, name, DOB) => {
+        localStorage.setItem('Name', name);
+        localStorage.setItem('DOB', DOB);
+        localStorage.setItem('Id', id);
+
+    }
+
+    const handleDelete = (id) => {
+        var index = Employees.map(function(e) {
+            return e.id
+        }).indexOf(id);
+
+        Employees.splice(index,1);
+
+        history('/');
+    }
+    
+    return(
+        <Fragment>
+
+        <div style={{margin:"10rem"}}>
+            <Table striped bordered hover size="sm">
+                <thead>
+                    <tr>
+                        <th>
+                            Name
+                        </th>
+                        <th>
+                            DOB
+                        </th>
+                        <th>
+                            Actions
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        Employees && Employees.length > 0
+                        ?
+                        Employees.map((item) =>{
+                            return(
+                                <tr>
+                                    <td>
+                                         {item.Name}
+                                    </td>
+                                    <td>
+                                        {item.DOB}
+                                    </td>
+                                     <td>
+                                        <Link to={'/edit'}>
+                                        <Button onClick={() => handleEdit(item.id, item.Name, item.DOB)}>EDIT</Button>
+                                        </Link>
+                                        &nbsp;
+                                        <Button onClick={() => handleDelete(item.id)}>DELETE</Button>
+                                    </td>
+                                </tr>
+                            )
+                        })
+                        :
+                        "No data available"
+                    }
+                </tbody>
+            </Table>
+            <br>
+            </br>
+            <Link className='d-grid gap-2' to="/create">
+                <Button size="lg">Create</Button>
+            </Link>
+         </div>
+        </Fragment>
+    )
+}
+
+export default Home;
